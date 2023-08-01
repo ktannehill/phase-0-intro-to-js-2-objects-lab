59f80eb5928cf62cb265e4e33835a7c438233431
Kat Tannehill <katetannehill@gmail.com>
2023-07-31 19:38:26 -0600

    created functions to perform operations on objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sherlock Holmes",
+    streetAddress: "221B Baker St, London",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) { 
+    return { 
+           ...employee, 
+          [key]: value, 
+     }; 
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    // create a copy of employee; delete key
+    const newEmp = { ...employee.key }; 
+    // return employee
+    return newEmp;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
